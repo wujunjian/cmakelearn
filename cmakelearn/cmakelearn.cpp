5b33c8c7f8ad73d6089b2b050d7af56d92e1e4a0
武俊健 <56492724@qq.com>
2018-07-14 00:09:24 +0800

    first commit

---
+++ cmakelearn/cmakelearn.cpp
@@ -0,0 +1,26 @@
+﻿// cmakelearn.cpp: 定义应用程序的入口点。
+//
+
+#include "cmakelearn.h"
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <stdlib.h> 
+#include "hello.h"
+#include "world.h"
+using namespace std;
+
+int main()
+{
+	std::time_t t = std::time(nullptr);
+	cout << "Hello CMake。" << std::put_time(std::localtime(&t), "%c %Z") << endl;
+
+
+	hello()(string("hello"));
+	world()(string("world"));
+
+	cin.get();
+	return 0;
+}
---
+++ hello/hellp.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+class hello
+{
+public:
+	void operator()(string data) { cout << data << endl; }
+};
